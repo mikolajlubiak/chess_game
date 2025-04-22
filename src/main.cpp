482e05084adec8b53ee067de362965a6a01b9658
Mikołaj Lubiak <lubiak@proton.me>
2025-04-22 15:18:47 +0200

    Seperate en passant and castling logic

--- src/main.cpp
+++ src/main.cpp
@@ -83,82 +83,12 @@ void loop() {
       if (file >= 0 && file < 8 && rank >= 0 && rank < 8 &&
           newPositionBitboard & (moves | captures)) {
 
-        if (selectedPiece->type == PieceType::King) {
-          std::array<int8_t, 2> selectedPosition =
-              PositionFromBitboard(selectedBitboard);
-          std::array<int8_t, 2> newPosition =
-              PositionFromBitboard(newPositionBitboard);
-
-          if (newPosition[0] == selectedPosition[0] - 2) {
-            uint64_t rookBitboard = 0;
-
-            if (selectedPiece->color == PieceColor::White) {
-              rookBitboard = 1ULL << 56;
-            } else {
-              rookBitboard = 1ULL << 0;
-            }
-
-            std::array<int8_t, 2> rookPosition =
-                PositionFromBitboard(rookBitboard);
-
-            Piece &leftRook = GetPieceAt(rookBitboard, pieces);
-
-            leftRook.bitboard =
-                ((leftRook.bitboard) & ~rookBitboard) |
-                (1ULL << (rookPosition[1] * 8 + rookPosition[0] + 3));
-          } else if (newPosition[0] == selectedPosition[0] + 2) {
-            uint64_t rookBitboard = 0;
-
-            if (selectedPiece->color == PieceColor::White) {
-              rookBitboard = 1ULL << 63;
-            } else {
-              rookBitboard = 1ULL << 7;
-            }
-
-            std::array<int8_t, 2> rookPosition =
-                PositionFromBitboard(rookBitboard);
-
-            Piece &rightRook = GetPieceAt(rookBitboard, pieces);
-
-            rightRook.bitboard =
-                ((rightRook.bitboard) & ~rookBitboard) |
-                (1ULL << (rookPosition[1] * 8 + rookPosition[0] - 2));
-          }
-        }
-
-        if (newPositionBitboard & enPassantBitboard) {
-          for (Piece &piece : pieces) {
-            if (piece.bitboard & enPassantedBitboard) {
-              piece.bitboard = piece.bitboard & ~enPassantedBitboard;
-            }
-          }
-        }
-
-        enPassantBitboard = 0;
+        HandleCastling(*selectedPiece, selectedBitboard, newPositionBitboard, pieces);
+        HandleEnPassant(*selectedPiece, selectedBitboard, newPositionBitboard, enPassantBitboard, enPassantedBitboard, pieces);
 
         for (Piece &piece : pieces) {
           if (piece.bitboard & newPositionBitboard) {
             piece.bitboard = piece.bitboard & ~newPositionBitboard;
-          }
-        }
-
-        if (selectedPiece->type == PieceType::Pawn) {
-          int8_t diff;
-          if (selectedPiece->color == PieceColor::White) {
-            diff = -1;
-          } else {
-            diff = 1;
-          }
-
-          std::array<int8_t, 2> selectedPosition =
-              PositionFromBitboard(selectedBitboard);
-          std::array<int8_t, 2> newPosition =
-              PositionFromBitboard(newPositionBitboard);
-
-          if (newPosition[1] == selectedPosition[1] + diff * 2) {
-            enPassantBitboard = 1ULL << ((selectedPosition[1] + diff) * 8 +
-                                         selectedPosition[0]);
-            enPassantedBitboard = newPositionBitboard;
           }
         }
 
